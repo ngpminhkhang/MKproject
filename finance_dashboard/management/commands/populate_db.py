8a5539dcd465a18639c50a2e92e52f53752aa67b
ngpminhkhang <ngpminhkhang@gmail.com>
2025-09-13 00:26:19 +0700

    Add populate_db for superuser creation

--- finance_dashboard/management/commands/populate_db.py
+++ finance_dashboard/management/commands/populate_db.py
@@ -1,10 +1,10 @@
 import os
-from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
-from django.core.management import call_command
+     from django.contrib.auth.models import User
+     from django.core.management.base import BaseCommand
+     from django.core.management import call_command
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')).exists():
-            call_command('createsuperuser', '--noinput', '--username', os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin'), '--email', os.environ.get('DJANGO_SUPERUSER_EMAIL', 'abcadmin@gmail.com'))
-        self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+     class Command(BaseCommand):
+         def handle(self, *args, **options):
+             if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')).exists():
+                 call_command('createsuperuser', '--noinput', '--username', os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin'), '--email', os.environ.get('DJANGO_SUPERUSER_EMAIL', 'abcadmin@gmail.com'))
+             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
