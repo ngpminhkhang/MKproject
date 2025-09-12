e4f2d9dfa670d239602aeeb037c154f481af63ea
ngpminhkhang <ngpminhkhang@gmail.com>
2025-09-13 00:50:10 +0700

    Update populate_db with password for superuser

--- finance_dashboard/management/commands/populate_db.py
+++ finance_dashboard/management/commands/populate_db.py
@@ -5,6 +5,9 @@ import os
 
      class Command(BaseCommand):
          def handle(self, *args, **options):
-             if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')).exists():
-                 call_command('createsuperuser', '--noinput', '--username', os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin'), '--email', os.environ.get('DJANGO_SUPERUSER_EMAIL', 'abcadmin@gmail.com'))
+             username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+             email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'abcadmin@gmail.com')
+             password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Traucon123@')
+             if not User.objects.filter(username=username).exists():
+                 call_command('createsuperuser', '--noinput', username=username, email=email, password=password)
              self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
