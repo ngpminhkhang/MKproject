f6561a3d103d23b73f5dc8bc877e926fb7413f82
ngpminhkhang <ngpminhkhang@gmail.com>
2025-09-13 01:48:21 +0700

    Fix populate_db syntax and update email/password

--- finance_dashboard/management/commands/populate_db.py
+++ finance_dashboard/management/commands/populate_db.py
@@ -1,13 +1,13 @@
 import os
-     from django.contrib.auth.models import User
-     from django.core.management.base import BaseCommand
-     from django.core.management import call_command
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
 
-     class Command(BaseCommand):
-         def handle(self, *args, **options):
-             username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-             email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'abcadmin@gmail.com')
-             password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Traucon123@')
-             if not User.objects.filter(username=username).exists():
-                 call_command('createsuperuser', '--noinput', username=username, email=email, password=password)
-             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'ngpminhkhang@gmail.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Traucon123@')
+        if not User.objects.filter(username=username).exists():
+            call_command('createsuperuser', '--noinput', username=username, email=email, password=password)
+        self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
